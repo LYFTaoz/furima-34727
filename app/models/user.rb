250caea7fe341df048b7111417f9735c046ef1ec
LYFTaoz <lift4523@gmail.com>
2021-07-11 18:48:46 +0900

    usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ApplicationRecord
-  has_many :items
-  has_many :buys
-
-  validate :nicknaname, presence: true
-  validate :email, presence: true
-  validate :encrypted_passward, presence: true
-  validate :family_name, presence: true
-  validate :fast_name, presence: true
-  validate :family_name_kana, presence: true
-  validate :fast_name_kana, presence: true
-  validate :birth_day, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 end
